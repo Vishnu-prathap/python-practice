020b676b1da46b783e97a32374def767d6082475
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-10-23 01:16:14 +0530

    Initial commit

---
+++ sixdArray.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    result = float('-inf')
+
+    i,j=1,1
+
+    for n in range(16):
+        cur = 0
+
+        cur = arr[i][j] + arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+
+        result = max(result, cur)
+ 
+        j+=1
+
+        if j == 5:
+            i += 1
+            j = 1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
