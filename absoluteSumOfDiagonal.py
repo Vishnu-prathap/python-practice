cae921f2b33af925b8d08bd78575c0d0feaf6046
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-10-24 01:48:09 +0530

    IC: Can be done without the loop too

---
+++ absoluteSumOfDiagonal.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    diagnol1 = 0
+    diagnol2 = 0
+    for i in range(len(arr)):
+        diagnol1 += arr[i][i]
+        diagnol2 += arr[i][len(arr) - 1 -i]
+    return abs(diagnol2 - diagnol1)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
